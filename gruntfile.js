a440c1f8f82deda3473169ccde06d72a8a8e4b10
Thomas Deinhamer <thasmo@gmail.com>
2013-10-31 01:26:34 +0100

    Set up grunt configuration

--- gruntfile.js
+++ gruntfile.js
@@ -3,9 +3,49 @@ module.exports = function(grunt){
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
-
+		pkg: grunt.file.readJSON('package.json'),
+		watch: {
+			styles: {
+				files: ['application/**/*.css'],
+				tasks: ['cssmin']
+			},
+			scripts: {
+				files: ['application/**/*.js'],
+				tasks: ['uglify']
+			},
+			options: {
+				livereload: true
+			}
+		},
+		cssmin: {
+			main: {
+				files: {
+					'htdocs/static/style/application.css': [
+						'bower_components/normalize-css/normalize.css',
+						'application/asset/style/main.css'
+					]
+				}
+			}
+		},
+		uglify: {
+			main: {
+				files: {
+					'htdocs/static/script/application.js': [
+						'bower_components/jquery/jquery.js',
+						'application/asset/script/main.js'
+					]
+				}
+			}
+		},
+		copy: {
+			main: {
+				src: 'bower_components/html5shiv/dist/html5shiv.js',
+				dest: 'htdocs/static/script/vendor/',
+				expand: true,
+				flatten: true
+			}
+		}
 	});
 
-	grunt.registerTask('default', []);
+	grunt.registerTask('default', ['cssmin', 'uglify', 'copy']);
 };
