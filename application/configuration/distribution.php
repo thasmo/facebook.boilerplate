36daf94b3469716a346b3e9fdd075f5bc9c0756b
Thomas Deinhamer <thasmo@gmail.com>
2013-10-31 01:41:51 +0100

    Fix indentation in distributed configuration file

--- application/configuration/distribution.php
+++ application/configuration/distribution.php
@@ -1,28 +1,28 @@
 ;<?php exit(); /*
 
-debug					= true
-timezone				= "UTC"
+debug                = true
+timezone             = "UTC"
 
 [application]
-title					= "Facebook Boilerplate"
+title                = "Facebook Boilerplate"
 
 [facebook]
-appId					= ""
-secret					= ""
-channelUrl				= "//localhost/channel.html"
-scope					= "email"
-fileUpload				= false
-status					= true
-cookie					= true
-xfbml					= true
-logging					= true
-frictionlessRequests	= true
-hideFlashCallback		= null
-trustForwarded			= false
-canvasWidth				= "810"
-canvasHeight			= "1000"
+appId                = ""
+secret               = ""
+channelUrl           = "//localhost/channel.html"
+scope                = "email"
+fileUpload           = false
+status               = true
+cookie               = true
+xfbml                = true
+logging              = true
+frictionlessRequests = true
+hideFlashCallback    = null
+trustForwarded       = false
+canvasWidth          = "810"
+canvasHeight         = "1000"
 
 [tracking]
-key						= "UA-undefined"
+key                  = "UA-undefined"
 
 ;*/
